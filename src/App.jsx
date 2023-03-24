2aa53aa006701ab8b960c4c93a4ebd53bdda7b79
Youssef Baataoui <ybaataoui@golynx.com>
2023-03-24 16:54:44 -0400

    Add API url to fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+const API_KEY = import.meta.env.VITE_APP_API_KEY; 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState(null)
+console.log(list)
+useEffect(() => {
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://gateway.marvel.com:443/v1/public/characters?ts=youssef&apikey=c7c1991fa21a226b7171cb60cf347ad2&hash=47022c672cd961043e5d2b4ce44db3a5" );
+
+      const json = await response.json();
+      setList(json)
+  }
+  fetchData().catch(console.error)
+
+},[])
 
   return (
     <div className="App">
